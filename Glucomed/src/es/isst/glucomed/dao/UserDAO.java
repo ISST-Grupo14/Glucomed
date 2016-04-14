8066ccfe4b908f283b7cef091a9c9261b8e8305f
aligc <alicia.gonzalez.cardenas@alumnos.upm.es>
2016-04-14 20:50:10 +0200

    Login funcionando. Añadido model, dao y servlet de Paciente. Primera versión del dashboard.

--- Glucomed/src/es/isst/glucomed/dao/UserDAO.java
+++ Glucomed/src/es/isst/glucomed/dao/UserDAO.java
@@ -1,7 +1,5 @@
 package es.isst.glucomed.dao;
 
-import es.isst.glucomed.model.*;
-
 public interface UserDAO {
 	
 	public boolean createUser(String nombre, String apellidos, String password, String email);
