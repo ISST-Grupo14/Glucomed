b177acadde0718d5a4705828971805b9a5696eba
sanxlop <sanchez.11.93@gmail.com>
2016-04-29 12:20:35 +0200

    Read en SuccessUpload, imprime csv por consola

---
+++ Glucomed/src/es/isst/glucomed/files/FileFunctions.java
@@ -0,0 +1,16 @@
+package es.isst.glucomed.files;
+
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+
+import com.opencsv.CSVReader;
+
+public class FileFunctions {
+
+
+
+}
--- Glucomed/src/es/isst/glucomed/files/SuccessUploadFileServlet.java
+++ Glucomed/src/es/isst/glucomed/files/SuccessUploadFileServlet.java
@@ -1,13 +1,20 @@
 package es.isst.glucomed.files;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreInputStream;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.opencsv.CSVReader;
 
 public class SuccessUploadFileServlet extends HttpServlet {
 	private BlobstoreService blobstoreService = BlobstoreServiceFactory
@@ -22,6 +29,36 @@ public class SuccessUploadFileServlet extends HttpServlet {
 		res.setContentType("text/html");
 		res.getWriter().println("Successfully uploaded. Download file: <br/>");
 		res.getWriter().println("<a href='/serve?blob-key=" + blobKey + "'>Click to download</a>");
+
+		BlobstoreInputStream is = new BlobstoreInputStream(new BlobKey(blobKey));
+		
+		/*de cada linea, lee los huecos 0 1 y 2 ---> "hola","que tal","adios",
+	     *y luego le la siguiente linea
+
+		 CSVReader reader = new CSVReader(new InputStreamReader(is));
+		
+	     String [] nextLine;
+	     while ((nextLine = reader.readNext()) != null) {
+	        System.out.println(nextLine[0] + nextLine[1] + nextLine[2]);
+	     }
+	     */
+	     
+	     
+		CSVReader reader = new CSVReader(new InputStreamReader(is));
+		ArrayList<String[]> csvList = new ArrayList<String[]>(reader.readAll());
+
+		// las i recorren las lineas
+		// las j recorren los huecos dentro de las lineas
+		for (int i = 0; i < csvList.size(); i++) {
+			for (int j = 0; j < csvList.get(i).length; j++)
+				System.out.println(csvList.get(i)[j]);
+		}
+
+		reader.close();
+		
+	
+		
+		
 
 	}
 }
